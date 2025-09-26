99163b32a5e089c4fd3fca8afc248f1a802bbb19
Inias Peeters <inias.peeters@axxes.com>
2025-09-26 08:01:11 +0200

    Revert main method changes so spring-boot-maven-plugin finds main class

--- src/main/java/eu/inias/ddnsoperator/DdnsOperatorApplication.java
+++ src/main/java/eu/inias/ddnsoperator/DdnsOperatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DdnsOperatorApplication {
 
-	void main(String[] args) {
+	public static void main(String[] args) {
 		System.setProperty("java.net.preferIPv4Stack" , "true");
 		SpringApplication.run(DdnsOperatorApplication.class, args);
 	}
